24d638c8ea2253008dcd71c3a0aeabf50f4a40cf
edmundo papi <edmundopierre@yahoo.com>
2020-04-11 17:29:51 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
 class Owner
   # code goes here
+  attr_accessor :name,:species
+
+ @@all = [] 
+ def initialize (name)
+   @name = name
+   @mood = "nervous"
+   @@all << self
+   @species="human" 
+   @@all << self
+ end
+
+ def say_species
+   self.species
+ end
+
+ def self.all
+   @@all
+ end
+
+ def self.count
+    self.all.count
+ end
+
+ def self.reset_all
+   self.all.clear
+ end
 end
